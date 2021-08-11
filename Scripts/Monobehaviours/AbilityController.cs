9c4960f303efee2f40d80005b5fa1cf8c2034b44
Roland Chelwing <jolleee@hotmail.com>
2021-08-11 16:32:17 +0200

    Update AbilityController.cs

--- Scripts/Monobehaviours/AbilityController.cs
+++ Scripts/Monobehaviours/AbilityController.cs
@@ -5,104 +5,89 @@ using UnityEngine.InputSystem;
 
 namespace Fralle.AbilitySystem
 {
-  public class AbilityController : MonoBehaviour
-  {
-    [field: SerializeField]
-    public ActiveAbility MovementAbility { get; private set; }
+	public class AbilityController : MonoBehaviour
+	{
+		[field: SerializeField]
+		public ActiveAbility MovementAbility { get; private set; }
 
-    [field: SerializeField]
-    public ActiveAbility AttackAbility { get; private set; }
+		[field: SerializeField]
+		public ActiveAbility AttackAbility { get; private set; }
 
-    [field: SerializeField]
-    public ActiveAbility UltimateAbility { get; private set; }
+		[field: SerializeField]
+		public ActiveAbility UltimateAbility { get; private set; }
 
-    public List<PassiveAbility> PassiveAbilities;
+		public List<PassiveAbility> PassiveAbilities;
 
-    [HideInInspector] public PostProcessController postProcessController;
+		[HideInInspector] public PostProcessController postProcessController;
 
-    PlayerInput playerInput;
+		public void NewAbility(Ability ability, AbilityType abilityType)
+		{
+			switch (abilityType)
+			{
+				case AbilityType.Movement:
+					MovementAbility = (ActiveAbility)SetupAbility(ability);
+					break;
+				case AbilityType.Attack:
+					AttackAbility = (ActiveAbility)SetupAbility(ability);
+					break;
+				case AbilityType.Ultimate:
+					UltimateAbility = (ActiveAbility)SetupAbility(ability);
+					break;
+				case AbilityType.Passive:
+					PassiveAbilities.Add((PassiveAbility)SetupAbility(ability));
+					break;
+			}
+		}
 
-    public void NewAbility(Ability ability, AbilityType abilityType)
-    {
-      switch (abilityType)
-      {
-        case AbilityType.Movement:
-          MovementAbility = (ActiveAbility)SetupAbility(ability);
-          break;
-        case AbilityType.Attack:
-          AttackAbility = (ActiveAbility)SetupAbility(ability);
-          break;
-        case AbilityType.Ultimate:
-          UltimateAbility = (ActiveAbility)SetupAbility(ability);
-          break;
-        case AbilityType.Passive:
-          PassiveAbilities.Add((PassiveAbility)SetupAbility(ability));
-          break;
-      }
-    }
+		protected virtual void Awake()
+		{
+			postProcessController = GetComponentInChildren<PostProcessController>();
 
-    void Awake()
-    {
-      postProcessController = GetComponentInChildren<PostProcessController>();
+			SetupAbilities();
+		}
 
-      SetupAbilities();
-
-      playerInput = GetComponent<PlayerInput>();
-      playerInput.actions["MovementAbility"].performed += OnMovementAbility;
-      playerInput.actions["AttackAbility"].performed += OnAttackAbility;
-      playerInput.actions["UltimateAbility"].performed += OnUltimateAbility;
-
-    }
-
-    void SetupAbilities()
-    {
-      MovementAbility = (ActiveAbility)SetupAbility(MovementAbility);
-      AttackAbility = (ActiveAbility)SetupAbility(AttackAbility);
-      UltimateAbility = (ActiveAbility)SetupAbility(UltimateAbility);
+		void SetupAbilities()
+		{
+			MovementAbility = (ActiveAbility)SetupAbility(MovementAbility);
+			AttackAbility = (ActiveAbility)SetupAbility(AttackAbility);
+			UltimateAbility = (ActiveAbility)SetupAbility(UltimateAbility);
 
 
-      List<PassiveAbility> temp = new List<PassiveAbility>();
-      foreach (PassiveAbility ability in PassiveAbilities)
-      {
-        PassiveAbility instance = (PassiveAbility)SetupAbility(ability);
-        temp.Add(instance);
-      }
-      PassiveAbilities = temp;
-    }
+			List<PassiveAbility> temp = new List<PassiveAbility>();
+			foreach (PassiveAbility ability in PassiveAbilities)
+			{
+				PassiveAbility instance = (PassiveAbility)SetupAbility(ability);
+				temp.Add(instance);
+			}
+			PassiveAbilities = temp;
+		}
 
-    Ability SetupAbility(Ability ability)
-    {
-      if (ability == null)
-        return null;
+		Ability SetupAbility(Ability ability)
+		{
+			if (ability == null)
+				return null;
 
-      Ability instance = Instantiate(ability);
-      instance.Setup(this);
-      return instance;
-    }
+			Ability instance = Instantiate(ability);
+			instance.Setup(this);
+			return instance;
+		}
 
-    void OnMovementAbility(InputAction.CallbackContext context)
-    {
-      if (MovementAbility != null && MovementAbility.IsReady)
-        MovementAbility.Perform();
-    }
+		void OnMovementAbility(InputAction.CallbackContext context)
+		{
+			if (MovementAbility != null && MovementAbility.IsReady)
+				MovementAbility.Perform();
+		}
 
-    void OnAttackAbility(InputAction.CallbackContext context)
-    {
-      if (AttackAbility != null && AttackAbility.IsReady)
-        AttackAbility.Perform();
-    }
+		void OnAttackAbility(InputAction.CallbackContext context)
+		{
+			if (AttackAbility != null && AttackAbility.IsReady)
+				AttackAbility.Perform();
+		}
 
-    void OnUltimateAbility(InputAction.CallbackContext context)
-    {
-      if (UltimateAbility != null && UltimateAbility.IsReady)
-        UltimateAbility.Perform();
-    }
-
-    void OnDestroy()
-    {
-      playerInput.actions["MovementAbility"].performed -= OnMovementAbility;
-      playerInput.actions["AttackAbility"].performed -= OnAttackAbility;
-      playerInput.actions["UltimateAbility"].performed -= OnUltimateAbility;
-    }
-  }
+		void OnUltimateAbility(InputAction.CallbackContext context)
+		{
+			if (UltimateAbility != null && UltimateAbility.IsReady)
+				UltimateAbility.Perform();
+		}
+	}
 }
